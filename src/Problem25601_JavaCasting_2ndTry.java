9188a07ae350563692e0013d192ad181c913df77
melamond <mikisa0813@gmail.com>
2023-04-14 16:36:19 +0900

    Problem 25601 2nd try in progress

--- src/Problem25601_JavaCasting_2ndTry.java
+++ src/Problem25601_JavaCasting_2ndTry.java
@@ -1,19 +1,20 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Problem25601_JavaCasting_2ndTry {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // br, st 선언
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
 
         // A와 B를 받을 Hashmap 생성
         HashMap<String, String> map = new HashMap<String, String>();
 
-
-
-
+        // A와 B를 받는 st
+        st = new StringTokenizer(br.readLine());
+        map.put(st.nextToken(), st.nextToken());
     }
 }

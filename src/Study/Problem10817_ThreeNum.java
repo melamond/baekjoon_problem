16e117a555f7bd704a4659a8c24cfce957a2dde9
melamond <mikisa0813@gmail.com>
2023-04-24 19:46:15 +0900

    Study 10817 세 수 Solved

---
+++ src/Study/Problem10817_ThreeNum.java
@@ -0,0 +1,23 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem10817_ThreeNum {
+    public static void main(String[] args) throws IOException {
+        int[] arr = new int[3];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 3; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.print(arr[1]);
+    }
+}
